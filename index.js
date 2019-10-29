2c186c8bb7cffb828b595f825d92335ab098216e
Nicole Dow <nikki.dow71@gmail.com>
2019-10-29 16:31:00 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
-// Add your functions here
+function map(array, fn) {
+    let transformed = [];
+
+    array.forEach(ele => {
+      transformed.push(fn(ele))
+    })
+    return transformed;
+}
+
+function reduce(array, fn, start = 0) {
+    if (start) {
+        let accumulator = start
+    } else {
+        let accumulator = array[0]
+    }
+
+    
+}
