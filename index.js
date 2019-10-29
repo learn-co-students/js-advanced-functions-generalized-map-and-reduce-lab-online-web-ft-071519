10a06fc7d4594e0f96d8ba3a426127f3af111305
Audrea Cook <audreacwork@gmail.com>
2019-10-29 15:40:06 -0600

    Done.

--- index.js
+++ index.js
@@ -8,7 +8,8 @@ const map = function(array, func) {
 
 const reduce = function(array, func, starting) {
     if (!starting) {
-        starting = 0
+        starting = array[0];
+        array = array.splice(1)
     }
     let result = starting;
     for (const element of array) {
@@ -16,43 +17,3 @@ const reduce = function(array, func, starting) {
     }
     return result
 };
-
-
-// // reduceToTotal returns a running total when not given a starting point
-// // reduceToTotal returns a running total when given a starting point
-
-// const reduceToTotal = function(array, startingPoint = 0) {
-//     let result = startingPoint;
-//     for (const element of array) {
-//         total += element
-//     }
-//     return result
-// };
-//
-// // reduceToAllTrue returns true when all values are truthy
-// // reduceToAllTrue returns false when any value is falsy
-
-// const reduceToAllTrue = function (array) {
-//     let result = true;
-//     for (const element of array) {
-//         if (element == false) {
-//             result = false;
-//             break;
-//         }
-//     }
-//     return result
-// };
-//
-// // reduceToAnyTrue returns true when a true value is present
-// // reduceToAnyTrue returns false when no true value is present
-
-// const reduceToAnyTrue = function (array) {
-//     let result = false;
-//     for (const element of array) {
-//         if (element == true) {
-//             result = true;
-//             break;
-//         }
-//     }
-//     return result
-// };
