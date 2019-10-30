bbf8db913441c4c4af34eb689f270a3c0c357869
radville <laura.radville@gmail.com>
2019-10-29 20:55:01 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
-// Add your functions here
+function map(sourceArray, fcn) {
+    let newArray = [];
+    sourceArray.forEach(element => {
+        newArray.push(fcn(element))
+    });
+    return newArray
+}
+
+function reduce(sourceArray, fcn, startingValue = 0) {
+    if (sourceArray.every(function(element){return typeof element === "number"})){
+        let total = startingValue
+        sourceArray.forEach(element => {
+            total = fcn(element, total);
+          });
+          return total
+    } else {
+        let total = true
+        sourceArray.forEach(element => {
+            total = fcn(element, total);
+          });
+          return total
+    }
+}
